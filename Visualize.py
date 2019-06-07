8da1c3681b0613929d63705cba3ce0ab6ebd2a36
ChiChaChai <chatchaishaetan@gmail.com>
2019-06-08 00:45:17 +0700

    Improve visualization

--- Visualize.py
+++ Visualize.py
@@ -2,18 +2,27 @@ import cv2
 import numpy as np
 
 
-def visualize( keypoints):
+def visualize(keypoints, image=None, actual_class = '',pred_class=''):
     vertexs = ((0, 1), (0, 15), (0, 16), (0, 17),
                (0, 18), (1, 2), (1, 5), (1, 8), (2, 3), (3, 4), (5, 6), (6, 7), (8, 9), (8, 12), (9, 10), (10, 11), (11, 22), (11, 24), (12, 13), (13, 14), (14, 19), (14, 21), (15, 17), (16, 18), (19, 20), (22, 23))
     keypoints = keypoints.reshape((25, 2))
-    width, height = (128, 128)
-    image = np.zeros((height, width))
+
+    if image is None:
+        height,  width = (128, 128)
+        image = np.zeros((height, width))
+    else:
+        height, width = image.shape[:2]
+
     for vertex in vertexs:
-        p1 = tuple((keypoints[vertex[0]] * (width, height)).astype('int'))
-        p2 = tuple((keypoints[vertex[1]] * (width, height)).astype('int'))
+        p1 = tuple((keypoints[vertex[0]]).astype('int'))
+        p2 = tuple((keypoints[vertex[1]]).astype('int'))
         cv2.line(image, p1, p2, (255, 255, 255))
     for keypoint in keypoints:
-        c = tuple((keypoint*np.array([width, height])).astype('int'))
+        c = tuple((keypoint).astype('int'))
         cv2.circle(image, c, 2, (0, 0, 255), 2)
-    return image
     
+    cv2.putText(image,'actual '+ actual_class, (0, height-50),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(image, 'predict '+pred_class, (0, height-1),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    return image
--- test.py
+++ test.py
@@ -3,19 +3,29 @@ import DataGenerator
 import Visualize
 import numpy as np
 import cv2
+from functools import reduce
 
 model = load_model('model.h5')
-x_test,y_test = DataGenerator.load_dataset(1,number_sample=100)
-y_pred = model.predict(x_test,)
+x_test, y_test, actions = DataGenerator.load_dataset(
+    1, number_sample=100, load_image=True)
 
+samples = []
+for action in actions:
+    samples.extend(action.samples)
+
+actions[0].samples[1].raw_keypoints
+
+y_pred = model.predict(x_test,)
 for i in range(len(x_test)):
     x = x_test[i]
     y = y_pred[i]
     y = np.argmax(y)
+    label_pred = DataGenerator.categories[y]
+    label_true = samples[i].name.split('/')[0]
 
-    print(y)
-    
-    image = Visualize.visualize(x[0])
-    cv2.imshow('skeleton',image)
-    cv2.waitKey(10)
-    cv2.destroyAllWindows()
+    image = Visualize.visualize(
+        samples[i].raw_keypoints, np.copy(samples[i].image), actual_class=label_true, pred_class=label_pred)
+    cv2.imshow('skeleton', image)
+    if cv2.waitKey(10) == ord('q'):
+        break
+cv2.destroyAllWindows()
